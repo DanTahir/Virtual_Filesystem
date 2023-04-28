402ab5985a053e5ed918ed061a8e1a4068d75b84
Chris Camano <chriscamano007@gmail.com>
2023-04-27 19:16:56 -0700

    Small change to isvalidfile

--- b_io.c
+++ b_io.c
@@ -52,7 +52,8 @@ void b_init ()
 
 //A small helper function for file descriptor validation
 bool isValidFileDescriptor(b_io_fd fd) {
-    return (fd >= 0) && (fd < MAXFCBS);
+	if (fcbArray[fd].buf==NULL) return false;
+    else return (fd >= 0) && (fd < MAXFCBS);
 }
 
 int handleFileNotFound(b_io_fd returnFd, DirEntry *dir, char *realFileName, int flags) {
@@ -157,7 +158,7 @@ b_io_fd b_open (char * filename, int flags)
 	dirCopyWorking(&dir);
 	char realFileName[NAMELEN];
 	int traverseReturn = dirTraversePath(&dir, filename, realFileName);
-	
+
 	if(traverseReturn == -1){
 		printf("invalid path\n");
 		return -1;
